c0602a2f2355eb4baf60b1c17870868898b928f7
Parul Bansal <bparul999@gmail.com>
2019-11-16 01:10:18 +0530

    party

---
+++ app/src/main/java/com/example/event/User_table.java
@@ -0,0 +1,64 @@
+package com.example.event;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class User_table extends SQLiteOpenHelper {
+    public static final String DATABASE_1 = "USER.db";
+    public static final String TABLE_1 = "user_table";
+    public static final String col0 = "UserName";
+    public static final String col1 = "Id";
+    public static final String col2 = "email";
+
+    public User_table (@Nullable Context context) {
+        super(context, DATABASE_1, null, 4);
+    }
+
+    public int adddata(String uname, String email) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(col0,uname);
+        contentValues.put(col2,email);
+
+        long result = db.insert(TABLE_1, null, contentValues);
+        if (result == -1)
+            return 0;
+        else
+            return 1;
+    }
+    public int query (String email) {
+        int ans = 0;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = db.rawQuery("SELECT * FROM " +
+                        TABLE_1, null);
+        if(data.getCount()>0){
+            if(data.moveToFirst()){
+                do{
+                    if (data.getString(2).matches(email)) {
+                        ans = Integer.parseInt(data.getString(0));
+                        return ans;
+                    }
+                }while (data.moveToNext());
+            }
+        }
+        return ans;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_1 + "("
+                + col1 + " INTEGER PRIMARY KEY AUTOINCREMENT," + col0 + " VARCHAR,"
+                + col2 + " VARCHAR UNIQUE"+")";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/example/event/team_table.java
@@ -0,0 +1,62 @@
+package com.example.event;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class team_table extends SQLiteOpenHelper {
+    public static final String DATABASE_1 = "TEAM.db";
+    public static final String TABLE_1 = "team";
+    public static final String col0 = "teamname";
+    public static final String col1 = "teamid";
+
+    public team_table(@Nullable Context context) {
+        super(context, DATABASE_1, null, 2);
+    }
+
+    public int query (String tname) {
+        int ans = 0;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = db.rawQuery("SELECT * FROM " + TABLE_1 , null);
+        if(data.getCount()>0){
+            if(data.moveToFirst()){
+                do{
+                    if (data.getString(0).matches(tname)) {
+                        ans = Integer.parseInt(data.getString(1));
+                        return ans;
+                    }
+                }while (data.moveToNext());
+            }
+        }
+        return ans;
+    }
+
+    public int adddata(String tname) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(col0, tname);
+
+        long result = db.insert(TABLE_1, null, contentValues);
+        if (result == -1)
+            return 0;
+        else
+            return 1;
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_1 + "("
+                + col1 + " INTEGER PRIMARY KEY AUTOINCREMENT," + col0 + " INTEGER UNIQUE"
+                +" )";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
